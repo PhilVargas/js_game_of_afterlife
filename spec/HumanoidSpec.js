191c3aec7fac462cc9fb253b4884518e0e8f82ca
Phil V <philipavargas@gmail.com>
2014-03-31 15:10:22 -0700

    add testing for #getBitten add testing for #bite

--- spec/HumanoidSpec.js
+++ spec/HumanoidSpec.js
@@ -1,10 +1,13 @@
 describe("Humanoid", function(){
   var human;
-
+  var zombie;
+  var infected;
   describe("A human", function(){
 
     beforeEach(function(){
       human = new Humanoid({'speed': 10, 'humanType': 'human', 'position': {'x': 20, 'y': 20}});
+      zombie = new Humanoid({'speed': 5, 'humanType': 'zombie'});
+      infected = new Humanoid({'speed': 5, 'humanType': 'infectedHuman'});
     });
 
     it("should have a default position", function(){
@@ -36,12 +39,10 @@ describe("Humanoid", function(){
       });
 
       it("should not be attracted to a zombie", function(){
-        var zombie = new Humanoid({'speed': 5, 'humanType': 'zombie'});
         expect(human.isAttractedTo(zombie)).toEqual(false)
       });
 
       it("should not be attracted to an infectedHuman", function(){
-        var infected = new Humanoid({'speed': 5, 'humanType': 'infectedHuman'});
         expect(human.isAttractedTo(infected)).toEqual(false)
       });
     });
@@ -69,6 +70,51 @@ describe("Humanoid", function(){
         expect(human.isLastMoveRepeated(distantPosition)).toEqual(false)
       });
     });
+
+    describe("#getBitten", function(){
+      beforeEach(function(){
+        human.getBitten()
+      })
+
+      it("should change the humantype to an infectedHuman", function(){
+        expect(human.humanType).toEqual('infectedHuman')
+      })
+
+      it("should set the speed of the humanoid to 0", function(){
+        expect(human.speed).toEqual(0)
+      })
+    })
+
+    describe("#bite", function(){
+      describe("a human", function(){
+        beforeEach(function(){
+          zombie.bite(human)
+        })
+        it("should change the humantype to an infectedHuman", function(){
+          expect(human.humanType).toEqual('infectedHuman')
+        })
+
+        it("should set the speed of the humanoid to 0", function(){
+          expect(human.speed).toEqual(0)
+        })
+      })
+
+      describe("a zombie", function(){
+        beforeEach(function(){
+          anotherZombie = new Humanoid({'speed': 5, 'humanType': 'zombie'});
+          zombie.bite(anotherZombie)
+        })
+        it("should not change the humantype to an infectedHuman", function(){
+          expect(anotherZombie.humanType).toEqual('zombie')
+        })
+
+        it("should not set the speed of the humanoid to 0", function(){
+          expect(anotherZombie.speed).not.toEqual(0)
+        })
+      })
+    })
+
+    
 
   });
 });
