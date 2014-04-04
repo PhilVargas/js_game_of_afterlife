09186033ce1a439be6237ac171c9b24f6dfb5799
hpchess <hkpaull@ucdavis.edu>
2014-04-04 00:02:40 -0700

    improved the settings object to contain more extracted settings. remove the hard coding of these extracted settings. add some comments to the settings file to explain some of the less clear settings.

--- public/js/settings.js
+++ public/js/settings.js
@@ -4,5 +4,12 @@ var gameSettings = {
   humanCount: 30,
   zombieCount: 3,
   turnDelay: 100,
-  repitionTolerance: .3
+  //sets the timeout between turns
+  repitionTolerance: 1,
+  //the range in which a move is considered repetitive
+  //lower values will reduce the size of the range.
+  zombieSpread: 6,
+  //lower zombieSpread values will cause zombies to spread out more
+  humanFearRange: 80
+  //the range at which humans start running from zombies.
 };
