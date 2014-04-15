390477e0dcde09c3836fe1f7d434d62913414def
Phil V <philipavargas@gmail.com>
2014-04-15 12:56:36 -0700

    fix findsimilarhumanoids to output only humanoids with the same humanType prevent error when the last humanoid is consumed

--- public/js/settings.js
+++ public/js/settings.js
@@ -8,8 +8,8 @@ var gameSettings = {
   repitionTolerance: 1,
   //the range in which a move is considered repetitive
   //lower values will reduce the size of the range.
-  zombieSpread: 6,
+  zombieSpread: 3,
   //lower zombieSpread values will cause zombies to spread out more
-  humanFearRange: 80
+  humanFearRange: 20
   //the range at which humans start running from zombies.
 };
