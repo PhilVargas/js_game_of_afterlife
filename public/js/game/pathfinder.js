d714c2157b31d9aa468dfdddc80ea731a2056237
Phil Vargas <philipavargas@gmail.com>
2015-08-13 19:42:56 -0700

    chore(Pathfinder) lint and convert minor es6 syntax

--- public/js/game/pathfinder.js
+++ public/js/game/pathfinder.js
@@ -1,23 +1,16 @@
 let Pathfinder = {
   moveTowards: function(currentPosition, friendlyLocation, speed){
-    var deltaY = friendlyLocation.y - currentPosition.y;
-    var deltaX = friendlyLocation.x - currentPosition.x;
-    var length = Pathfinder.distanceTo(friendlyLocation, currentPosition);
+    let deltaY, deltaX, length;
+    deltaY = friendlyLocation.y - currentPosition.y;
+    deltaX = friendlyLocation.x - currentPosition.x;
+    length = Pathfinder.distanceTo(friendlyLocation, currentPosition);
     if (speed !== 0 && length < speed){
       return friendlyLocation
     } else {
-    return {'x': (currentPosition.x + (deltaX / length * speed)),'y': (currentPosition.y + (deltaY / length * speed))}
-    }
-  },
-
-  movePerpendicularTo: function(currentPosition, friendlyLocation, speed){
-    var deltaY = friendlyLocation.y - currentPosition.y;
-    var deltaX = friendlyLocation.x - currentPosition.x;
-    var length = Pathfinder.distanceTo(friendlyLocation, currentPosition);
-    if (speed !== 0 && length < speed){
-      return friendlyLocation
-    } else {
-      return {'x': (currentPosition.x + (deltaX / length * speed)),'y': (currentPosition.y - (deltaY / length * speed))}
+      return {
+        x: (currentPosition.x + (deltaX / length * speed)),
+        y: (currentPosition.y + (deltaY / length * speed))
+      }
     }
   },
 
@@ -25,16 +18,35 @@ let Pathfinder = {
     return Pathfinder.moveTowards(currentPosition, hostileLocation, -speed);
   },
 
+  movePerpendicularTo: function(currentPosition, friendlyLocation, speed){
+    let deltaY, deltaX, length;
+    deltaY = friendlyLocation.y - currentPosition.y;
+    deltaX = friendlyLocation.x - currentPosition.x;
+    length = Pathfinder.distanceTo(friendlyLocation, currentPosition);
+    if (speed !== 0 && length < speed){
+      return friendlyLocation
+    } else {
+      return {
+        x: (currentPosition.x + (deltaX / length * speed)),
+        y: (currentPosition.y - (deltaY / length * speed))
+      }
+    }
+  },
+
   distanceTo: function(targetLocation, currentPosition){
-    var deltaY = targetLocation.y - currentPosition.y;
-    var deltaX = targetLocation.x - currentPosition.x;
+    let deltaY, deltaX;
+    deltaY = targetLocation.y - currentPosition.y;
+    deltaX = targetLocation.x - currentPosition.x;
     return Math.sqrt(Math.pow(deltaY,2) + Math.pow(deltaX,2))
   },
 
   moveRandomly: function(currentPosition, speed){
     let angle;
     angle = Math.random() * 2 * Math.PI;
-      return {'x': (currentPosition.x + Math.cos(angle) * speed),'y': (currentPosition.y + Math.sin(angle) * speed)}
+      return {
+        x: (currentPosition.x + Math.cos(angle) * speed),
+        y: (currentPosition.y + Math.sin(angle) * speed)
+      }
   },
 }
 
