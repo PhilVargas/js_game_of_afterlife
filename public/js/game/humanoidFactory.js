4cd46ba88e7c3bda8b44e6f1b76671c21712546b
Phil Vargas <philipavargas@gmail.com>
2015-08-13 19:26:58 -0700

    chore(humanoidFactory) lint humanoid factor and add minor es6 syntax

--- public/js/game/humanoidFactory.js
+++ public/js/game/humanoidFactory.js
@@ -3,15 +3,20 @@ gameSettings = require('settings')
 Humanoid = require('humanoid')
 
 let HumanoidBuilder = {
-  populate: function(numberOfHumans, numberOfZombies){
-    return HumanoidBuilder.creation(numberOfHumans, 'human', gameSettings.humanSpeed).concat(HumanoidBuilder.creation(numberOfZombies, 'zombie', gameSettings.zombieSpeed )).concat(HumanoidBuilder.creation(1, 'player', gameSettings.playerSpeed))
+  populate(numberOfHumans, numberOfZombies){
+    return (
+      HumanoidBuilder.creation(numberOfHumans, 'human', gameSettings.humanSpeed)
+        .concat(HumanoidBuilder.creation(numberOfZombies, 'zombie', gameSettings.zombieSpeed))
+        .concat(HumanoidBuilder.creation(1, 'player', gameSettings.playerSpeed))
+    )
   },
 
-  creation: function(number, type, speed){
-    var population = []
-    for(var i = 0; i < number; i++){
-      var newHumanoid = new Humanoid({'humanType': type, 'speed': speed})
-      population.push(newHumanoid)
+  creation(number, type, speed){
+    let population, newHumanoid;
+    population = [];
+    for(let i = 0; i < number; i++){
+      newHumanoid = new Humanoid({'humanType': type, 'speed': speed});
+      population.push(newHumanoid);
     }
     return population
   }
