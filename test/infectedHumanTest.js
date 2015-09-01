cac4a3a5b8b40cd3d5ab829ba8b2d81e13d22740
Phil Vargas <philipavargas@gmail.com>
2015-09-01 07:57:28 -0700

    test(infectedHuman) add initial tests for infectedHuman

---
+++ test/infectedHumanTest.js
@@ -0,0 +1,41 @@
+require('babel/register');
+var chai, sinon, expect;
+chai = require('chai');
+sinon = require('sinon');
+expect = chai.expect;
+
+var Zombie, Infected, gameSettings, Pathfinder;
+
+Infected = require('humanoids/infectedHuman');
+Zombie = require('humanoids/zombie');
+gameSettings = require('settings');
+Pathfinder = require('pathfinder');
+
+describe('InfectedHuman', function(){
+  var zombie, infected;
+
+  describe('An infectedHuman', function(){
+    beforeEach(function(){
+      infected = new Infected({id: 0});
+    });
+
+    it('is an InfectedHuman `#humanType`', function(){
+      expect(infected.humanType).to.equal('InfectedHuman');
+    });
+
+    it('has a speed of 0', function(){
+      expect(infected.speed).to.equal(0);
+    });
+
+    it('has a `timeSinceInfection` of 0', function(){
+      expect(infected.speed).to.equal(0);
+    });
+  });
+
+  describe('#isAbleToBite', function(){
+    it('is false', function(){
+      expect(infected.isAbleToBite()).to.equal(false);
+    });
+  });
+});
+
