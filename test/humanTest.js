6ba27607dac3bb9649c81eae7f5391bbaa3cd8c0
Phil Vargas <philipavargas@gmail.com>
2015-09-02 09:01:50 -0700

    test(human) add test for `handleNextMove`

--- test/humanTest.js
+++ test/humanTest.js
@@ -1,6 +1,7 @@
 let chai, sinon, expect;
 chai = require('chai');
 sinon = require('sinon');
+chai.use(require('chai-changes'));
 expect = chai.expect;
 
 let Human, InfectedHuman, gameSettings, Pathfinder;
@@ -11,7 +12,7 @@ gameSettings = require('settings');
 Pathfinder = require('pathfinder');
 
 describe('Human', function(){
-  let zombie, human;
+  let human;
 
   describe('A human', function(){
     beforeEach(function(){
@@ -36,6 +37,50 @@ describe('Human', function(){
   describe('#transform', function(){
     it('returns an instance of InfectedHuman', function(){
       expect(human.transform()).to.be.an.instanceOf(InfectedHuman);
+    });
+  });
+
+  describe('#handleNextMove', function(){
+    let opts, destination;
+    afterEach(function(){
+      human.getNextDestination.restore();
+      human.isValidDestination.restore();
+    });
+
+    context('when the next move is not valid', function(){
+      beforeEach(function(){
+        destination = { x: 5, y: 5 };
+        sinon.stub(human, 'getNextDestination');
+        sinon.stub(human, 'isValidDestination').returns(false);
+        opts = {getRelativePosition: sinon.stub().returns(destination)};
+      });
+
+      it('sets the `human.position` to the destination', function(){
+        expect(function(){
+          return human.position;
+        }).to.not.change.when(function(){
+          human.handleNextMove(opts);
+        });
+      });
+    });
+
+    context('when the next move is valid', function(){
+      beforeEach(function(){
+        destination = { x: 5, y: 5 };
+        sinon.stub(human, 'getNextDestination');
+        sinon.stub(human, 'isValidDestination').returns(true);
+        opts = {getRelativePosition: sinon.stub().returns(destination)};
+      });
+
+      it('sets the `human.position` to the destination', function(){
+        expect(function(){
+          return human.position;
+        }).to.change.to(
+          destination
+        ).when(function(){
+          human.handleNextMove(opts);
+        });
+      });
     });
   });
 });
