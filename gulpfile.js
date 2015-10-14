e8c7534c5cc295275c7818cf6ccc4b6bad848d06
Phil Vargas <philipavargas@gmail.com>
2015-10-14 11:42:55 -0800

    chore(gulpfile) add backwards compatible gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,17 @@ var gulp, gulpTasks;
 gulp = require('gulp');
 gulpTasks = require('./public/js/gulp/game_of_afterlife.js');
 
+gulp.task('js:build', gulpTasks.build.js);
+gulp.task('js:watch', gulpTasks.watch.js);
 gulp.task('build:js', gulpTasks.build.js);
 gulp.task('watch:js', gulpTasks.watch.js);
 
+gulp.task('sass:build', gulpTasks.build.sass);
+gulp.task('sass:watch', gulpTasks.watch.sass);
 gulp.task('build:sass', gulpTasks.build.sass);
 gulp.task('watch:sass', gulpTasks.watch.sass);
 
+gulp.task('assets:build', ['build:js', 'build:sass']);
+gulp.task('assets:watch', ['watch:js', 'watch:sass']);
 gulp.task('build:assets', ['build:js', 'build:sass']);
 gulp.task('watch:assets', ['watch:js', 'watch:sass']);
